88d78e1360182a724d0e6ff96fd2fe7a331cfe30
Veillard10 <202502857012@alunos.estacio.br>
2025-10-29 07:49:44 +0000

    torre

--- xadrez.c
+++ xadrez.c
@@ -14,8 +14,47 @@ int main(){
     =================================================================
     */  
 
-    // Torre = For + While
+    // Torre = For + While 
+    // Movimento = Linha horizontal e vertical
 
+    int TorresCasa = 5
     
+    printf("========Torre========\n");
+    
+    
+    for (int i = 1, i <= TorresCasa; i++) {
+        printf("Torre moveu-se %d para Direita\n");
+
+    }
+
+
+
+
+        /* 
+    ==============================================================
+    OBJETIVO 1
+    
+    Estrutura de repetição siples (For,While e Do-While) 
+    
+    =================================================================
+    */  
+
+    // Torre = For + While 
+    // Movimento = Linha horizontal e vertical
+
+    }
+
+     
+
+
+
+
+
+
+
+    }
+
+
+
+
     return 0;
-}
